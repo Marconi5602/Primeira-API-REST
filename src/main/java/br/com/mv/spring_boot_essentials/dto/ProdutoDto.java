0ceb15de60ec4fe1989d3fc270b29d0c0b49777f
Marconi Vianna <mvs2006.mv@gmail.com>
2026-08-26 14:51:59 -0300

    commit inicial - API CRUD de produtos

---
+++ src/main/java/br/com/mv/spring_boot_essentials/dto/ProdutoDto.java
@@ -0,0 +1,21 @@
+package br.com.mv.spring_boot_essentials.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ProdutoDto {
+
+    private String nome;
+    private BigDecimal preco;
+    private Integer quantidade;
+
+
+}
